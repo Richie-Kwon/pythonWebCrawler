33bc4fe3eeeac70d8033897dbb521b917b15cc4d
richie <richie.kwon1@gmail.com>
2021-10-13 19:59:51 +0100

    string

---
+++ pythonFile.py
@@ -0,0 +1,13 @@
+# Data Checking : type(variable) string/ boolean/ dic/ tuple
+# Calculation : // quotient % reminder ** square
+# Changing data type: int(figure), float(figure), str(figure)
+# y +=1
+
+# String len(string)
+# String escape escape = "Do you have \"a big party\""
+# String escape escape2 = "using tab \t using tab \t"
+# String Raw raw = r'file path'
+# String multi-line multi = """ string multi lines """
+
+
+   
